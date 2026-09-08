98d1bb5bbf9456a3e613f3b7c474d72a299f4753
prithvi-01x <169276772+prithvi-01x@users.noreply.github.com>
2026-09-08 12:07:48 +0530

    feat(leetcode): solve 84 - Largest Rectangle in Histogram

---
+++ leetcode84.cpp
@@ -0,0 +1,27 @@
+#include <vector>
+#include <stack>
+#include <algorithm>
+
+using namespace std;
+
+class Solution {
+public:
+    int largestRectangleArea(vector<int>& heights) {
+        int n = heights.size();
+        stack<int> st;
+        int maxArea = 0;
+
+        for (int i = 0; i <= n; ++i) {
+            int currentHeight = (i == n) ? 0 : heights[i];
+            while (!st.empty() && currentHeight < heights[st.top()]) {
+                int h = heights[st.top()];
+                st.pop();
+                int width = st.empty() ? i : (i - st.top() - 1);
+                maxArea = max(maxArea, h * width);
+            }
+            st.push(i);
+        }
+
+        return maxArea;
+    }
+};
